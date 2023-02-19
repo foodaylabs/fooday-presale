5d163f6aec866f49eb42e2ca0388b29fe1c6dc13
0xthk <kf013099@gmail.com>
2023-02-19 09:03:53 +0900

    fix: twitter link

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 const config = {
   LANDING_URL: 'https://fooday.app',
   DOCS_URL: 'https://docs.google.com/presentation/d/11C2RwS5EwNushHBQoy2Z622zhiBRdzVh40rRIrFUJxk',
-  TWITTER_URL: 'https://twitter.com/fooday',
+  TWITTER_URL: 'https://twitter.com/foodayapp',
   DISCORD_URL: 'https://discord.gg/6Q3Xa7fRV4',
   MEDIUM_URL: 'https://medium.com/@fooday',
   IG_URL: 'https://www.instagram.com/fooday.app/',
